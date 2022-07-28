8280a0e7f0824609ac718c39b2ef42fe14f5c4e7
slattgram <outta.hoodz@gmail.com>
2022-07-28 20:37:50 +0300

    added todo description pages (routes), cant delete bc of no db

--- src/app/todo-description/todo-description.component.ts
+++ src/app/todo-description/todo-description.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {todos,Todo} from "../todo";
 
 @Component({
   selector: 'app-todo-description',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoDescriptionComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,) {
   }
 
+  id: any;
+  todo: Todo | any;
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const todoIdFromRoute = Number(routeParams.get('todoId'));
+    this.todo = todos.find(todo => todo.id === todoIdFromRoute)
+  }
+
+  //working, but cant display properly bc of no db
+  deleteItemById() {
+
+    for (let i = 0; i < todos.length; i++) {
+
+      if (this.todo.id === todos[i].id) {
+
+        todos.splice(i, 1);
+
+      }
+    }
+    console.log(todos)
+  }
 }
